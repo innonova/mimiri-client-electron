d5503d9fe76923a3b0738063d57c6d272bb5ee50
Anders Eggers-Krag <aek@innonova.ch>
2024-11-26 08:42:03 +0100

    windows support

--- scripts/rename-packages.js
+++ scripts/rename-packages.js
@@ -3,14 +3,30 @@ const { readFileSync, writeFileSync } = require('node:fs');
 
 const package = JSON.parse(readFileSync('./package.json'))
 
-shell.mv('./dist/mimiri-notes.tar.gz', `./dist-bin/mimiri-notes_${package.version}_amd64.tar.gz`)
-shell.mv(`./dist/mimiri-notes_${package.version}_amd64.snap`, `./dist-bin/mimiri-notes_${package.version}_amd64.snap`)
-shell.mv(`./dist/Mimiri Notes-${package.version}.AppImage`, `./dist-bin/mimiri-notes_${package.version}_amd64.AppImage`)
-shell.mv('./flatpak/io.mimiri.notes.flatpak', `./dist-bin/io.mimiri.notes_${package.version}_amd64.flatpak`)
+if (process.platform === 'linux') {
+	shell.mv('./dist/mimiri-notes.tar.gz', `./dist-bin/mimiri-notes_${package.version}_amd64.tar.gz`)
+	shell.mv(`./dist/mimiri-notes_${package.version}_amd64.snap`, `./dist-bin/mimiri-notes_${package.version}_amd64.snap`)
+	shell.mv(`./dist/Mimiri Notes-${package.version}.AppImage`, `./dist-bin/mimiri-notes_${package.version}_amd64.AppImage`)
+	shell.mv('./flatpak/io.mimiri.notes.flatpak', `./dist-bin/io.mimiri.notes_${package.version}_amd64.flatpak`)
 
-writeFileSync('./artifacts.json', JSON.stringify([
-	`dist-bin/mimiri-notes_${package.version}_amd64.tar.gz`,
-	`dist-bin/mimiri-notes_${package.version}_amd64.snap`,
-	`dist-bin/mimiri-notes_${package.version}_amd64.AppImage`,
-	`dist-bin/io.mimiri.notes_${package.version}_amd64.flatpak`,
-], undefined, '  '))
+	writeFileSync('./artifacts.json', JSON.stringify([
+		`dist-bin/mimiri-notes_${package.version}_amd64.tar.gz`,
+		`dist-bin/mimiri-notes_${package.version}_amd64.snap`,
+		`dist-bin/mimiri-notes_${package.version}_amd64.AppImage`,
+		`dist-bin/io.mimiri.notes_${package.version}_amd64.flatpak`,
+	], undefined, '  '))
+}
+
+if (process.platform === 'windows') {
+	const electronWinInstallerPath = './out/make/squirrel.windows/x64'
+	const artifacts = []
+	for (const file of readdirSync(electronWinInstallerPath)) {
+		if (file.endsWith('.exe')) {
+			artifacts.push(Path.join(electronWinInstallerPath, file))
+		}
+		if (file.endsWith('.nupkg')) {
+			artifacts.push(Path.join(electronWinInstallerPath, file))
+		}
+	}
+	writeFileSync('./artifacts.json', JSON.stringify(artifacts, undefined, '  '))
+}

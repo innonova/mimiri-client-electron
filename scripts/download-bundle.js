b5d497abe286aade35d17fb835d49f452cd73d3f
Anders Eggers-Krag <aek@innonova.ch>
2024-12-01 07:45:25 +0100

    stop fetchning bundle over scp

--- scripts/download-bundle.js
+++ scripts/download-bundle.js
@@ -8,12 +8,7 @@ const doc = yaml.load(readFileSync('./flatpak/io.mimiri.notes.yml'));
 const source = doc.modules.find(m => m.name === 'mimiri-notes').sources.find(s => s['dest-filename'] === 'bundle.json');
 
 console.log(`Download bundle from ${source.url}`)
-if (process.platform === 'darwin') {
-	const urlParts = source.url.split('/')
-	shell.exec(`scp ${process.env.SCP_BUNDLE_SRC}/${urlParts[urlParts.length - 1]} ./bundle.json`)
-} else {
-	shell.exec(`curl ${source.url} -o ./bundle.json`)
-}
+shell.exec(`curl ${source.url} -o ./bundle.json`)
 
 const res = shell.exec(`sha256sum ./bundle.json`)
 const hash = /\w+/.exec(res.stdout)[0]

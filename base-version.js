e2b7381ba8cdab4fb41a63729b4071e5dffdf4b8
Anders Eggers-Krag <aek@innonova.ch>
2024-12-03 14:34:26 +0100

    v2.1.1 - update bundle

--- base-version.js
+++ base-version.js
@@ -1,7 +1,7 @@
 
 		module.exports = {
-			baseVersion: '2.1.0',
-			hostVersion: '2.1.0',
-			releaseDate: '2024-12-03T09:08:01.465Z',
+			baseVersion: '2.1.1',
+			hostVersion: '2.1.1',
+			releaseDate: '2024-12-03T13:32:40.225Z',
 		}
 		

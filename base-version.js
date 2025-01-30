eb1711931ebdb6919788313347db2f9dc0d4742f
Anders Eggers-Krag <aek@innonova.ch>
2025-01-30 15:04:43 +0100

    extract flatpack

--- base-version.js
+++ base-version.js
@@ -1,7 +1,7 @@
 
 		module.exports = {
-			baseVersion: '2.1.70',
+			baseVersion: '2.1.78',
 			hostVersion: '2.1.70',
-			releaseDate: '2025-01-28T09:30:49.954Z',
+			releaseDate: '2025-01-29T07:25:15.774Z',
 		}
 		
--- generate-sources.sh
+++ generate-sources.sh
@@ -1,4 +1,4 @@
+npm install --lockfile-version 2
 mv ./node_modules ./node_modules.bak
 flatpak-node-generator npm package-lock.json
 mv ./node_modules.bak ./node_modules
-mv ./generated-sources.json ./flatpak/generated-sources.json

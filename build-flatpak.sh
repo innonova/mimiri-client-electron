63a7eb537aedd7713a52c44e1a1e3ddc5cc88489
Anders Eggers-Krag <aek@innonova.ch>
2025-01-30 15:50:16 +0100

    add debug

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -3,6 +3,8 @@ cd mimiri-flatpak
 git add .
 git commit -m"update generated-sources"
 git push
+lf -al
 flatpak-builder build io.mimiri.notes.yml --install-deps-from=flathub --force-clean --user --repo=flatpak-repo --install
+lf -al
 flatpak build-bundle flatpak-repo io.mimiri.notes.flatpak io.mimiri.notes --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
 cd ..
--- generate-sources.sh
+++ generate-sources.sh
@@ -1,6 +1,4 @@
 npm install --lockfile-version 2
 mv ./node_modules ./node_modules.bak
-which flatpak-node-generator
-ls /home/inno/.local/bin/
 /home/inno/.local/bin/flatpak-node-generator npm package-lock.json
 mv ./node_modules.bak ./node_modules

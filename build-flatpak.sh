963358fbd3efe39583176554c24b85b25bb11604
Anders Eggers-Krag <aek@innonova.ch>
2025-01-30 15:59:02 +0100

    remove debug

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -3,8 +3,6 @@ cd mimiri-flatpak
 git add .
 git commit -m"update generated-sources"
 git push
-lf -al
 flatpak-builder build io.mimiri.notes.yml --install-deps-from=flathub --force-clean --user --repo=flatpak-repo --install
-lf -al
 flatpak build-bundle flatpak-repo io.mimiri.notes.flatpak io.mimiri.notes --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
 cd ..

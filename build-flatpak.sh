28c071f474face1c00acd04d28a07ea714eca9f2
Anders Eggers-Krag <aek@innonova.ch>
2025-02-03 12:47:20 +0100

    include package lock in flatpak repo

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,6 +1,7 @@
 git clone git@github.com:innonova/mimiri-flatpak.git
 npm run update-flatpak
 cp ./generated-sources.json ./mimiri-flatpak/
+cp ./package-lock.json ./mimiri-flatpak/
 cd mimiri-flatpak
 arch=$(uname -m)
 if [ "$arch" != "aarch64" ]; then

5d14bd00e44e0cd464360f09150d5f4a0bf553f6
Anders Eggers-Krag <aek@innonova.ch>
2025-02-03 09:25:06 +0100

    do not update git from arm

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -2,9 +2,12 @@ git clone git@github.com:innonova/mimiri-flatpak.git
 npm run update-flatpak
 cp ./generated-sources.json ./mimiri-flatpak/
 cd mimiri-flatpak
-git add .
-git commit -m"update generated-sources and bundle"
-git push
+arch=$(uname -m)
+if [ "$arch" != "aarch64" ]; then
+	git add .
+	git commit -m"update generated-sources and bundle"
+	git push
+fi
 flatpak-builder build io.mimiri.notes.yml --install-deps-from=flathub --force-clean --user --repo=flatpak-repo --install
 flatpak build-bundle flatpak-repo io.mimiri.notes.flatpak io.mimiri.notes --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
 cd ..

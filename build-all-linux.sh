656caf0108d6728ff270810a58ae5fb49341609d
Anders Eggers-Krag <aek@innonova.ch>
2025-02-12 08:41:12 +0100

    fix arm build

--- build-all-linux.sh
+++ build-all-linux.sh
@@ -8,7 +8,7 @@ mkdir ./dist-bin
 npm run download-bundle
 npm run unpack-bundle -- ./bundle.json
 arch=$(uname -m)
-if [ "$arch" == "aarch64" ]; then
+if [ "$arch" = "aarch64" ]; then
 	sed -i '/allowNativeWayland/d' package.json
 fi
 npm run dist

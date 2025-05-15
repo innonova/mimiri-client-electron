fa2da038dc6ea24d64276a6880925ad174a95987
Anders Eggers-Krag <aek@innonova.ch>
2025-05-15 13:15:24 +0200

    debug flatpak

--- base-version.js
+++ base-version.js
@@ -1,7 +1,6 @@
 
-		module.exports = {
-			baseVersion: '2.1.105',
-			hostVersion: '2.1.103',
-			releaseDate: '2025-05-15T08:24:10.640Z',
-		}
-		
+module.exports = {
+	baseVersion: '2.1.105',
+	hostVersion: '2.1.105',
+	releaseDate: '2025-05-15T08:24:10.640Z',
+}

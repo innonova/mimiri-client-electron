f98a88ed966a834b6fa9176c56a485d40ee67e39
Anders Eggers-Krag <aek@innonova.ch>
2025-05-15 10:05:33 +0200

    new update system, test version

--- base-version.js
+++ base-version.js
@@ -1,7 +1,6 @@
 
-		module.exports = {
-			baseVersion: '2.1.102',
-			hostVersion: '2.1.101',
-			releaseDate: '2025-05-15T07:56:01.739Z',
-		}
-		
+module.exports = {
+	baseVersion: '2.1.102',
+	hostVersion: '2.1.102',
+	releaseDate: '2025-05-15T07:56:01.739Z',
+}

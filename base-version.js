4b80355d028be22afe9f1d6471dea3d7d4a63b46
Anders Eggers-Krag <aek@innonova.ch>
2025-05-19 10:00:48 +0200

    update version history

--- base-version.js
+++ base-version.js
@@ -1,7 +1,7 @@
 
 		module.exports = {
-			baseVersion: '2.3.0',
-			hostVersion: '2.3.0',
-			releaseDate: '2025-05-19T05:26:25.947Z',
+			baseVersion: '2.3.1',
+			hostVersion: '2.3.1',
+			releaseDate: '2025-05-19T07:58:45.436Z',
 		}
 		

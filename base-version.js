27cb99aeae22ea86be95d12148ce32e88db1dcc9
Anders Eggers-Krag <aek@innonova.ch>
2025-05-21 17:49:22 +0200

    fix new install detection

--- base-version.js
+++ base-version.js
@@ -1,7 +1,7 @@
 
 		module.exports = {
-			baseVersion: '2.3.14',
-			hostVersion: '2.3.2',
-			releaseDate: '2025-05-21T15:29:42.967Z',
+			baseVersion: '2.3.16',
+			hostVersion: '2.3.3',
+			releaseDate: '2025-05-21T15:48:37.794Z',
 		}
 		

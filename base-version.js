c5c1afde3f70883d507b5edb8840c77a86107d63
Anders Eggers-Krag <aek@innonova.ch>
2025-05-24 12:33:21 +0200

    update bundle

--- base-version.js
+++ base-version.js
@@ -1,7 +1,7 @@
 
 		module.exports = {
-			baseVersion: '2.3.25',
-			hostVersion: '2.3.25',
-			releaseDate: '2025-05-22T11:13:46.290Z',
+			baseVersion: '2.3.34',
+			hostVersion: '2.3.34',
+			releaseDate: '2025-05-24T09:46:09.798Z',
 		}
 		
---
+++ build-snap-arm.sh
@@ -0,0 +1,12 @@
+npm install
+rm -rf ./app
+rm -f ./bundle.json
+rm -rf ./dist
+rm -rf ./dist-bin
+mkdir ./dist-bin
+npm run download-bundle
+npm run unpack-bundle -- ./bundle.json
+npm run arm-wayland
+npm run dist
+npm run rename-packages
+ls -al ./dist-bin

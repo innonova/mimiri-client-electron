930cd1ac04788d10b75fc196230e8f99371faa0a
Anders Eggers-Krag <aek@innonova.ch>
2025-08-25 06:57:09 +0200

    v2.5.12

--- base-version.js
+++ base-version.js
@@ -1,7 +1,7 @@
 
 		module.exports = {
-			baseVersion: '2.4.33',
-			hostVersion: '2.4.33',
-			releaseDate: '2025-06-17T07:24:06.826Z',
+			baseVersion: '2.5.12',
+			hostVersion: '2.5.12',
+			releaseDate: '2025-08-25T04:52:23.557Z',
 		}
 		

bcd606b8113618615fba6c7b631907b169fc9d8c
Anders Eggers-Krag <aek@innonova.ch>
2025-10-06 12:58:47 +0200

    update bundle

--- src/base-version.ts
+++ src/base-version.ts
@@ -4,9 +4,9 @@ export interface BaseVersion {
 	releaseDate: string;
 }
 
-export const baseVersion = '2.5.39';
-export const hostVersion = '2.5.41';
-export const releaseDate = '2025-10-06T07:29:00.194Z';
+export const baseVersion = '2.5.40';
+export const hostVersion = '2.5.43';
+export const releaseDate = '2025-10-06T10:56:47.497Z';
 
 export default {
 	baseVersion,

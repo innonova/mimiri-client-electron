68ae049ff010b4f33c3946d2efb758408940d2eb
Anders Eggers-Krag <aek@innonova.ch>
2025-10-08 09:59:31 +0200

    bump bundle

--- src/base-version.ts
+++ src/base-version.ts
@@ -4,9 +4,9 @@ export interface BaseVersion {
 	releaseDate: string;
 }
 
-export const baseVersion = '2.5.65';
-export const hostVersion = '2.5.53';
-export const releaseDate = '2025-10-08T07:20:29.502Z';
+export const baseVersion = '2.5.68';
+export const hostVersion = '2.5.68';
+export const releaseDate = '2025-10-08T07:56:34.984Z';
 
 export default {
 	baseVersion,

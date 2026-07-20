f950b07056768fc1f307043b996b95dcbfa5c356
Anders Eggers-Krag <aek@innonova.ch>
2026-07-20 18:25:06 +0000

    2.6.16

--- src/base-version.ts
+++ src/base-version.ts
@@ -4,9 +4,9 @@ export interface BaseVersion {
 	releaseDate: string;
 }
 
-export const baseVersion = '2.6.9';
-export const hostVersion = '2.6.15';
-export const releaseDate = '2026-07-11T19:08:42.131Z';
+export const baseVersion = '2.6.16';
+export const hostVersion = '2.6.16';
+export const releaseDate = '2026-07-20T18:23:05.316Z';
 
 export default {
 	baseVersion,
